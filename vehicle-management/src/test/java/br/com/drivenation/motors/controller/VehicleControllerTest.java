ccf9730eb2b1e2b4e82c384110c99140b54464fb
gustavodiniz <gustavodinniz@hotmail.com>
2023-07-28 15:42:05 -0300

    feat: implementing all UT for VehicleController

--- vehicle-management/src/test/java/br/com/drivenation/motors/controller/VehicleControllerTest.java
+++ vehicle-management/src/test/java/br/com/drivenation/motors/controller/VehicleControllerTest.java
@@ -1,12 +1,16 @@
 package br.com.drivenation.motors.controller;
 
 import br.com.drivenation.motors.dto.request.CreateVehicleRequest;
+import br.com.drivenation.motors.dto.request.UpdateVehicleRequest;
 import br.com.drivenation.motors.dto.response.GetAllVehicleResponse;
+import br.com.drivenation.motors.dto.response.GetVehicleByIdResponse;
+import br.com.drivenation.motors.dto.response.UpdateVehicleResponse;
 import br.com.drivenation.motors.enums.VehicleStatus;
 import br.com.drivenation.motors.service.VehicleService;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -23,6 +27,8 @@ class VehicleControllerTest {
 
     private CreateVehicleRequest createVehicleRequest;
 
+    private UpdateVehicleRequest updateVehicleRequest;
+
     @Test
     void shouldCreateVehicleSuccessfully() {
         givenCreateVehicleRequest();
@@ -38,8 +44,56 @@ class VehicleControllerTest {
         thenExpectVehicleServiceGetAllVehiclesCalledOnce();
     }
 
+    @Test
+    void shouldDeleteVehicleSuccessfully() {
+        givenVehicleServiceDeleteVehicle();
+        whenDeleteVehicleCalled();
+        thenExpectVehicleServiceDeleteVehicleCalledOnce();
+    }
+
+    @Test
+    void shouldUpdateVehicleSuccessfully() {
+        givenUpdateVehicleRequest();
+        givenVehicleServiceUpdateVehicle();
+        whenUpdateVehicleCalled();
+        thenExpectVehicleServiceUpdateVehicleCalledOnce();
+    }
+
+    @Test
+    void shouldGetVehicleByIdSuccessfully() {
+        givenVehicleServiceGetVehicleById();
+        whenGetVehicleByIdCalled();
+        thenExpectVehicleServiceGetVehicleByIdCalledOnce();
+    }
 
     // given
+    private void givenVehicleServiceGetVehicleById() {
+        doReturn(GetVehicleByIdResponse.builder().build()).when(vehicleService).getVehicleById(any(ObjectId.class));
+    }
+
+    private void givenVehicleServiceUpdateVehicle() {
+        doReturn(UpdateVehicleResponse.builder().build()).when(vehicleService).updateVehicle(any(ObjectId.class), any(UpdateVehicleRequest.class));
+    }
+
+    private void givenUpdateVehicleRequest() {
+        updateVehicleRequest = UpdateVehicleRequest.builder()
+                .model("Uno")
+                .year(2021)
+                .color("Red")
+                .manufacturer("Fiat")
+                .chassisNumber("123456789")
+                .price(10000.00)
+                .status(VehicleStatus.UNDER_MAINTENANCE)
+                .build();
+    }
+
+    private void givenVehicleServiceCreateVehicle() {
+        doNothing().when(vehicleService).createVehicle(any(CreateVehicleRequest.class));
+    }
+
+    private void givenVehicleServiceDeleteVehicle() {
+        doNothing().when(vehicleService).deleteVehicle(any(ObjectId.class));
+    }
 
     private void givenVehicleServiceGetAllVehicles() {
         doReturn(List.of(GetAllVehicleResponse.builder().build())).when(vehicleService).getAllVehicles();
@@ -57,11 +111,35 @@ class VehicleControllerTest {
                 .build();
     }
 
-    private void givenVehicleServiceCreateVehicle() {
-        doNothing().when(vehicleService).createVehicle(any(CreateVehicleRequest.class));
+    // when
+
+    private void whenGetVehicleByIdCalled() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id", "64c1d11f7846e2f23389e96e")
+                .when()
+                .get("/vehicles/{id}")
+                .then()
+                .statusCode(200);
     }
 
-    // when
+    private void whenUpdateVehicleCalled() {
+        given()
+                .pathParam("id", "64c1d11f7846e2f23389e96e")
+                .contentType(ContentType.JSON)
+                .body(updateVehicleRequest)
+                .when().put("/vehicles/{id}")
+                .then()
+                .statusCode(200);
+    }
+
+    private void whenDeleteVehicleCalled() {
+        given()
+                .pathParam("id", "64c1d11f7846e2f23389e96e")
+                .when().delete("/vehicles/{id}")
+                .then()
+                .statusCode(204);
+    }
 
     private void whenGetAllVehiclesCalled() {
         given()
@@ -82,12 +160,24 @@ class VehicleControllerTest {
 
     // then
 
+    private void thenExpectVehicleServiceUpdateVehicleCalledOnce() {
+        verify(vehicleService).updateVehicle(any(ObjectId.class), any(UpdateVehicleRequest.class));
+    }
+
+    private void thenExpectVehicleServiceDeleteVehicleCalledOnce() {
+        verify(vehicleService).deleteVehicle(any(ObjectId.class));
+    }
+
     private void thenExpectVehicleServiceGetAllVehiclesCalledOnce() {
         verify(vehicleService).getAllVehicles();
     }
 
     private void thenExpectVehicleServiceCreateVehicleCalledOnce() {
         verify(vehicleService).createVehicle(any(CreateVehicleRequest.class));
+    }
+
+    private void thenExpectVehicleServiceGetVehicleByIdCalledOnce() {
+        verify(vehicleService).getVehicleById(any(ObjectId.class));
     }
 
 }
