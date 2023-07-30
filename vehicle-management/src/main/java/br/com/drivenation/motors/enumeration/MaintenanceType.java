a49edf623131afe0303b005bd1614ccd0f543095
gustavodiniz <gustavodinniz@hotmail.com>
2023-07-29 21:48:35 -0300

    feat: added price in MaintenanceType enum

--- vehicle-management/src/main/java/br/com/drivenation/motors/enumeration/MaintenanceType.java
+++ vehicle-management/src/main/java/br/com/drivenation/motors/enumeration/MaintenanceType.java
@@ -1,11 +1,24 @@
 package br.com.drivenation.motors.enumeration;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum MaintenanceType {
-    OIL_CHANGE,
-    ENGINE_REPAIR,
-    BRAKE_CHECK,
-    TIRE_ROTATION,
-    BATTERY_REPLACEMENT,
-    BODY_REPAIR
+
+    OIL_CHANGE(100.0),
+
+    ENGINE_REPAIR(500.0),
+
+    BRAKE_CHECK(150.0),
+
+    TIRE_ROTATION(150.0),
+
+    BATTERY_REPLACEMENT(200.0),
+
+    BODY_REPAIR(400.0);
+
+    private final Double price;
 
 }
